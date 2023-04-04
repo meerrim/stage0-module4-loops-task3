01f1918a3caadecd60d64a81ff55cb79462cae4f
Meerim Sadykbekova <sadykbekova.m@gmail.com>
2023-04-04 22:59:41 +0600

    Completed all tasks.

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        long sum = 0;
+        long temp = 0;
+        for (int i = 1; i <= lengthOfLastNumber; i++) {
+            temp = temp * 10 + 9;
+            sum += temp;
+        }
+        System.out.println(sum);
     }
 }
